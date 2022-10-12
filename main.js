bff1f3342439e4e04f220c4652180ca24c176cd3
Enrique Pinzon <brayan99q@gmail.com>
2022-10-12 13:06:42 -0500

    desafio

---
+++ main.js
@@ -0,0 +1,18 @@
+let enriqueDev = [
+    personalInfo={
+    nombre:"Enrique", 
+    apellido:"Pinzon", 
+    edad:23 ,
+    developerWeb:true,},
+    date=["21/06/1999"],
+    favoriteBook={
+        title:"Satanas",
+        autor:"Mario Mendoza",
+        date:2002,
+        url:"https://www.planetadelibros.com.co/libro-satanas/329985",
+    },
+    Hobbies=["playing videogames",],
+    function myFavoriteActivity(){
+        return "playing videogames"}
+]   
+console.log(enriqueDev[1])
